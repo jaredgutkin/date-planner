625af0c58912abba3a18c2cd2ac565b6d63c9e3e
Jared Gutkin <jgutkinsam@gmail.com>
2022-08-14 14:55:03 -0500

    edit notes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import CalendarComponent from "./components/Calendar";
 import Favorites from "./components/Favorites";
+import ListNotes from "./utils/ListNotes";
 import ToDo from "./components/ToDo"
+import EditNotes from "./utils/EditNotes";
 
 
 function App() {
   return (
     <div className="App">
       <h1>Hello</h1>
+      <ListNotes />
+      <EditNotes />
       <CalendarComponent />
       <Favorites />
       <ToDo />
